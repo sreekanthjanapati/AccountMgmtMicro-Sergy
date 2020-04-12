6615be298022c857ea9cbd311a8fae985cb6e982
sreekanthjanapati <sreekanth@test.com>
2020-04-12 22:48:50 +0530

    newly account mgmt project created

---
+++ src/main/java/com/microservices/AccountMgmtMicroSergyApplication.java
@@ -0,0 +1,13 @@
+package com.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountMgmtMicroSergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountMgmtMicroSergyApplication.class, args);
+	}
+
+}
